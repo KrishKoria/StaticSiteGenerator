c62fab70d6a1645043be4eca200d021d56d3d773
Krish Koria <krishkoria2004@gmail.com>
2025-01-10 17:08:36 +0000

    Add TextType enum and unit tests for TextNode class

--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,17 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGE = "image"
+    
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url

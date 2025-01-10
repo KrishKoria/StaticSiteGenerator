728aab83f241afadf1374bab0b98eb607bd51d7f
Krish Koria <krishkoria2004@gmail.com>
2025-01-10 17:35:23 +0000

    Add LeafNode class and unit tests for initialization and HTML rendering

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
-
+from htmlnode import HTMLNode, LeafNode
 class TestHTMLNode(unittest.TestCase):
     
     def test_props_to_html_single_prop(self):
@@ -14,6 +13,26 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_no_props(self):
         node = HTMLNode(tag='span', props={})
         self.assertEqual(node.props_to_html(), '')
+
+class TestLeafNode(unittest.TestCase):
+    
+    def test_leafnode_initialization(self):
+        node = LeafNode(tag='p', value='This is a paragraph.', props={'class': 'text'})
+        self.assertEqual(node.tag, 'p')
+        self.assertEqual(node.value, 'This is a paragraph.')
+        self.assertEqual(node.props, {'class': 'text'})
+    
+    def test_leafnode_to_html(self):
+        node = LeafNode(tag='p', value='This is a paragraph.', props={'class': 'text'})
+        self.assertEqual(node.to_html(), '<p class="text">This is a paragraph.</p>')
+    
+    def test_leafnode_to_html_no_props(self):
+        node = LeafNode(tag='p', value='This is a paragraph.', props={})
+        self.assertEqual(node.to_html(), '<p>This is a paragraph.</p>')
+    
+    def test_leafnode_to_html_no_tag(self):
+        node = LeafNode(tag=None, value='This is just text.', props={})
+        self.assertEqual(node.to_html(), 'This is just text.')
 
 if __name__ == '__main__':
     unittest.main()

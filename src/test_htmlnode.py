bc1b733723f04293828c5c786d16d61b2f074718
Krish Koria <krishkoria2004@gmail.com>
2025-01-10 17:15:58 +0000

    Add HTMLNode class and unit tests for props_to_html method

---
+++ src/test_htmlnode.py
@@ -0,0 +1,19 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    
+    def test_props_to_html_single_prop(self):
+        node = HTMLNode(tag='div', props={'class': 'container'})
+        self.assertEqual(node.props_to_html(), 'class="container"')
+    
+    def test_props_to_html_multiple_props(self):
+        node = HTMLNode(tag='a', props={'href': 'https://example.com', 'target': '_blank'})
+        self.assertEqual(node.props_to_html(), 'href="https://example.com" target="_blank"')
+    
+    def test_props_to_html_no_props(self):
+        node = HTMLNode(tag='span', props={})
+        self.assertEqual(node.props_to_html(), '')
+
+if __name__ == '__main__':
+    unittest.main()

0af7de7cef1b33753c031e2640ee4dbba9c41432
Krish Koria <krishkoria2004@gmail.com>
2025-03-05 17:23:35 +0000

    Update main script to support customizable basepath and adjust output directory

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace REPO_NAME with your actual GitHub repo name
+REPO_NAME="StaticSiteGenerator"
+
+# Run the main.py script with the basepath set to /REPO_NAME/
+python3 src/main.py "/${REPO_NAME}/"
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import os
 import shutil
+import sys
 from markdownblocks import markdown_to_html_node
+
 def main():
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     source_directory = "./static"
-    destination_directory = "./public"
+    destination_directory = "./docs"
     if os.path.exists(destination_directory):
         shutil.rmtree(destination_directory)
     
     copy_directory(source_directory, destination_directory)
     
-    generate_pages_recursive("./content", "template.html", "./public")
+    generate_pages_recursive("./content", "template.html", destination_directory, basepath)
 
 
 def copy_directory(src, dest):
@@ -35,7 +38,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("No h1 header found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, 'r') as file:
         markdown_content = file.read()
@@ -45,20 +48,21 @@ def generate_page(from_path, template_path, dest_path):
     html_content = markdown_to_html_node(markdown_content).to_html()
     title = extract_title(markdown_content)
     full_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    full_html = full_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, 'w') as file:
         file.write(full_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for root, dirs, files in os.walk(dir_path_content):
         for file in files:
             if file.endswith(".md"):
                 from_path = os.path.join(root, file)
                 relative_path = os.path.relpath(from_path, dir_path_content)
                 dest_path = os.path.join(dest_dir_path, os.path.splitext(relative_path)[0] + ".html")
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
 
 
 if __name__ == "__main__":
